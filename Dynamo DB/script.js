cbb3715a141c4de05a8d3997dee739aae6e67102
Shardul Lingwal <shardullingwal94@gmail.com>
2021-09-24 15:00:20 +0530

    parent project entry point and toc for each webpage

--- Dynamo DB/script.js
+++ Dynamo DB/script.js
@@ -1,2 +1,14 @@
 window.onload = function(e){ 
 }
+
+/* sidebar functions */
+function toggle_sidebar_topics() {
+  const sidebar_topics = document.getElementById("sidebar_topics");
+  if (sidebar_topics.style.display === 'none') {
+    document.getElementById("sidebar_topics_icon").innerHTML = "&times;";
+    sidebar_topics.style.display = "block";
+  } else {
+    document.getElementById("sidebar_topics_icon").innerHTML = "&#9776;";
+    document.getElementById("sidebar_topics").style.display = "none";
+  } 
+}

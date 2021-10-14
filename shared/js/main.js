3f14490483bae5d89d5d85cc0670ebcf2d7bace0
Shardul Lingwal <shardullingwal94@gmail.com>
2021-10-14 13:10:09 +0530

    added highlight.js and minor infra changes

---
+++ shared/js/main.js
@@ -0,0 +1,13 @@
+// function addGlobalScripts() {
+//   var script  = document.createElement('script');
+//   // script.src  = "../shared/highlight/highlight.min.js";
+//   script.src = "../highlight/highlight.min.js"
+//   script.type = 'text/javascript';
+//   script.defer = true;
+//   document.getElementsByTagName('head').item(0).appendChild(script);
+// };
+  
+// function invokeGlobalScripts() {
+//   hljs.highlightAll();
+// };
+  
